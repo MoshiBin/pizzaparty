c3481da862d75d57c14671a84d9f37bd73cf17d1
LiatSchulz <liat.schulz@gmail.com>
2014-11-14 01:55:49 +0200

    angullaar

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -3,6 +3,7 @@ var pizzaApp = angular.module('pizzaparty', []);
 pizzaApp.controller('MainCtrl', function ($scope, $http) {
   $http.get('/pizza').success(function(data) {
     $scope.members = data.members;
+
   });
   $scope.prettytoppings = function (toppings) {
       return toppings.join(", ");
