d0b8a9fe2d2c194ead83d64dd44b0b431d1f46d5
MoshiBin <MoshiBin@users.noreply.github.com>
2014-11-13 23:02:50 +0200

    Added mock pizza request

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, jsonify
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+@app.route("/pizza")
 def get_pizza():
-    return None 
+    return jsonify(members=[
+        {"name": "moshi", "slices": 1, "toppings": ["Plain", "Olives"]},
+    ])
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
