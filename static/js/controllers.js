b9b97f66988f04bc39d1cfb60ce5e831ffb98a5e
LiatSchulz <liat.schulz@gmail.com>
2014-11-14 03:18:55 +0200

    enter for adding...

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -31,3 +31,17 @@ pizzaApp.controller('MainCtrl', function ($scope, members) {
     members.addMember(angular.copy(newmember));
   };
 });
+
+pizzaApp.directive('ngEnter', function() {
+    return function(scope, element, attrs) {
+        element.bind("keydown keypress", function(event) {
+            if(event.which === 13) {
+                scope.$apply(function(){
+                    scope.$eval(attrs.ngEnter, {'event': event});
+                });
+
+                event.preventDefault();
+            }
+        });
+    };
+});
