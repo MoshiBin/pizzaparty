0fc0543ce68b659ece6296f2c2afbe963cbbaefb
MoshiBin <MoshiBin@users.noreply.github.com>
2014-11-14 02:53:07 +0200

    Added angular way for adding members

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -1,11 +1,33 @@
 var pizzaApp = angular.module('pizzaparty', []);
 
-pizzaApp.controller('MainCtrl', function ($scope, $http) {
-  $http.get('/pizza').success(function(data) {
-    $scope.members = data.members;
+pizzaApp.factory("members", function() {
+  var members = [];
+  var notifies = [];
 
+  return {
+    getMembers: function() {return members;},
+    subscribe: function(scope, callback) {
+      notifies.push(callback);
+    },
+    addMember: function(member) {
+      members.push(member);
+      notifies.forEach(function(notf) {
+        notf(member);
+      });
+    }
+  };
+});
+
+pizzaApp.controller('MainCtrl', function ($scope, members) {
+  members.subscribe($scope, function(member) {
+    $scope.members = members.getMembers();
   });
+
   $scope.prettytoppings = function (toppings) {
-      return toppings.join(", ");
-  }
+    return toppings.join(", ");
+  };
+
+  $scope.addMember = function(newmember) {
+    members.addMember(angular.copy(newmember));
+  };
 });
